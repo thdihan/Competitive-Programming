7fe31bad5ce2e08f7ba644e30687cb8132be406a
Tanvir Hossain Dihan <85718477+thdihan@users.noreply.github.com>
2021-07-18 01:01:26 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,12 @@
+/* Template : Tanvir Hossain Dihan (Still Updating) */
+
+#include <bits/stdc++.h>
+ 
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+int main()
+{
+    fastio;
+    
+    return 0;
+}
